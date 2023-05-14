bf3590bebbb4291dc12af257f7a813c0b0e078ac
hazinater641 <133593223+hazinater641@users.noreply.github.com>
2023-05-14 19:35:52 +0100

    Update get_and_set.sh
    plz

--- get_and_set.sh
+++ get_and_set.sh
@@ -3,7 +3,7 @@
 
 wget -q https://i.redd.it/29rztr2atai61.jpg
 mv 29rztr2atai61.jpg ~/Pictures/virtualinsanityBG.jpg
-gsettings set org.gnome.desktop.background picture-uri-dark file:///usr/share/backgrounds/virtualinsanityBG.jpg
+gsettings set org.gnome.desktop.background picture-uri-dark file:///~/Pictures/virtualinsanityBG.jpg
 echo " "
 echo "This desktop..."
 echo "Made of..."
