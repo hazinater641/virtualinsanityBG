12e3f6d1c31e00c18f31a45ab19a3ee4ef511f9c
hazinater641 <133593223+hazinater641@users.noreply.github.com>
2023-05-14 19:16:49 +0100

    Update get_and_set.sh
    quiet

--- get_and_set.sh
+++ get_and_set.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ## The script to download, rename and move the background and set the background
 
-wget https://i.redd.it/29rztr2atai61.jpg
-sudo mv 29rztr2atai61.jpg /usr/share/backgrounds/virtualinsanityBG.jpg
-gsettings set org.gnome.desktop.background picture-uri file://///usr/share/backgrounds/virtualinsanityBG.jpg
+wget -q https://i.redd.it/29rztr2atai61.jpg
+mv 29rztr2atai61.jpg ~/Pictures/virtualinsanityBG.jpg
+gsettings set org.gnome.desktop.background picture-uri file:///~/Pictures/virtualinsanityBG.jpg
 echo "This desktop..."
 echo "Made of..."
 echo "Virtual Insanity"
