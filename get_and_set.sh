8cc766bf5c6783f91a76899012b552f0a7c2f48e
hazinater641 <133593223+hazinater641@users.noreply.github.com>
2023-05-14 19:38:18 +0100

    Update get_and_set.sh
    omg it might be working

--- get_and_set.sh
+++ get_and_set.sh
@@ -2,8 +2,8 @@
 ## The script to download, rename and move the background and set the background
 
 wget -q https://i.redd.it/29rztr2atai61.jpg
-mv 29rztr2atai61.jpg ~/Pictures/virtualinsanityBG.jpg
-gsettings set org.gnome.desktop.background picture-uri-dark file:///~/Pictures/virtualinsanityBG.jpg
+mv 29rztr2atai61.jpg /home/$USER/Pictures/virtualinsanityBG.jpg
+gsettings set org.gnome.desktop.background picture-uri-dark file:///home/$USER/Pictures/virtualinsanityBG.jpg
 echo " "
 echo "This desktop..."
 echo "Made of..."
